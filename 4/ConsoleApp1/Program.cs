c06a087fec48c34f8b960d8d82b9dbb128a52dec
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-04-02 23:43:31 +0200

    Add files via upload

---
+++ 4/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using klasac;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            IKlasa klasa = new Klasa();
+            klasa.Test("Dziala OK");
+        }
+    }
+}
